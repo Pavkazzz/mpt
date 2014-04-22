851fd4b93df44f1e219ac0299b04398e3825e821
Pavkazzz <Pavkazzz@mail.ru>
2014-04-22 16:12:49 +0400

    Список учителей

--- calendar-cmd-line-sample/excel_parser/teacherparser.py
+++ calendar-cmd-line-sample/excel_parser/teacherparser.py
@@ -7,17 +7,26 @@ import re
 
 book = xlrd.open_workbook('2203_Raspisanie.xls')
 
+teacherlist = []
+
 for num in range(0, book._all_sheets_count):
     sheet = book.sheet_by_index(num)
 
     for row_index in range(11, sheet.nrows):
-    #for col_index in range(sheet.ncols):
-        col_index = 5  # 5 для 29 групп; 2 для 19 групп
-        cells = sheet.cell(row_index, col_index)
 
-        if isinstance(cells.value, float):
-            continue
+        for col_index in [2, 5]:
 
-        teacher = re.findall(ur'[А-я]\.\s*[А-я]\.\s*[-А-я]+', cells.value, flags=re.UNICODE)
-        for item in teacher:
-            print item.encode('utf-8')
+
+            #col_index = 5  # 5 для 29 групп; 2 для 19 групп
+            cells = sheet.cell(row_index, col_index)
+
+            if isinstance(cells.value, float):
+                continue
+
+            teacher = re.findall(ur'[А-я]\.\s*[А-я]\.\s*[-А-я]+', cells.value, flags=re.UNICODE)
+            for item in teacher:
+                if item not in teacherlist:
+                    teacherlist.append(item)
+
+for teacher in teacherlist:
+    print teacher
