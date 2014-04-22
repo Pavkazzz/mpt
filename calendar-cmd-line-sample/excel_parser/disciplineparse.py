7407ecbfeff4a11660980f4721a6dc9c9e79dd5e
Pavkazzz <Pavkazzz@mail.ru>
2014-04-22 16:25:58 +0400

    Список учителей

---
+++ calendar-cmd-line-sample/excel_parser/disciplineparse.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import xlrd
+import re
+#from sql import Teacher, Session
+
+
+book = xlrd.open_workbook('2203_Raspisanie.xls')
+
+disciplinelist = []
+
+#for num in range(0, book._all_sheets_count):
+sheet = book.sheet_by_index(2)
+
+for row_index in xrange(11, sheet.nrows-3):
+
+    for col_index in [2, 5]:
+
+
+        #col_index = 5  # 5 для 29 групп; 2 для 19 групп
+        cells = sheet.cell(row_index, col_index)
+
+        if isinstance(cells.value, float):
+            continue
+
+        teacher = re.split(ur'[А-я]\.\s*[А-я]\.\s*[-А-я]+', cells.value, flags=re.UNICODE)
+        for item in teacher:
+            if item not in disciplinelist and u',' not in item:
+                disciplinelist.append(item)
+
+for discipline in disciplinelist:
+    print discipline
